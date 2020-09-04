4c911c22bb5e0d68e2e1df2c9be56c39beeb3c17
Ben Dornis <ben@buildstarted.com>
2020-09-04 02:06:29 -0700

    svelte custom element weirdness

---
+++ src/tween-input.svelte
@@ -0,0 +1,29 @@
+<svelte:options tag="tween-input" />
+
+<script>
+	import { afterUpdate, beforeUpdate, onMount } from "svelte";
+	import { tweened, spring } from "svelte/motion";
+	import { cubicOut, linear, elasticIn, sineIn, quintIn } from "svelte/easing";
+
+	export let value = 0;
+
+	beforeUpdate(() => {
+		let val = document.createElement("div");
+		val.textContent = value == undefined ? "value is undefined (beforeUpdate)" : "value is " + value  + " (beforeUpdate)";
+		document.body.appendChild(val);
+	});
+	afterUpdate(() => {
+		let val = document.createElement("div");
+		val.textContent = value == undefined ? "value is undefined (afterUpdate)" : "value is " + value  + " (beforeUpdate)";
+		document.body.appendChild(val);
+	});
+	onMount(() => {	
+		let val = document.createElement("div");
+		val.textContent = value == undefined ? "value is undefined (onMount)" : "value is " + value  + " (beforeUpdate)";
+		document.body.appendChild(val);
+	});
+</script>
+
+<main>
+	<p>{value}</p>
+</main>
